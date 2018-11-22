52cf27d3e826d704caad780d755b82c07379432c
Unknown <oleggordiichenko@gmail.com>
2018-11-22 11:25:28 +0200

    autoload settings from conf.set: automode, img saving on/of

--- openCVlib/SoleGluingMachineImpruvements/app/controller.py
+++ openCVlib/SoleGluingMachineImpruvements/app/controller.py
@@ -17,7 +17,7 @@ from view import View
 from webcam import WebCam
 import imgRW
 from settings import Settings
-#import motion # #movement 21_11_18
+import motion # #movement 21_11_18
 
 import os
 # chose an implementation, depending on os
@@ -29,25 +29,41 @@ elif os.name == 'posix':
 else:
 	raise Exception("Sorry: no implementation for your platform ('%s') available" % os.name)
 
-Set = Settings('1.set')
+Set = Settings('conf.set')
 setStr = Set.load()
+# print("setStr len = %s" %len(setStr[0]))
+tmpLen = len(setStr[0]) # first setStr param is web cam res (640,480) or (640,480,15). 15 - fps. Check if camera supports it?
+if tmpLen == 2:
+	WebCamParam = [int(setStr[0][0]), int(setStr[0][1])] # [640, 480]
+	Camera = WebCam(0, WebCamParam[0], WebCamParam[1])
+elif tmpLen == 3:
+	WebCamParam = [int(setStr[0][0]), int(setStr[0][1]), int(setStr[0][2])]  # 160*120 - min HD - max e.x. [640, 360, 15]
+	Camera = WebCam(0, WebCamParam[0], WebCamParam[1], WebCamParam[2])
 
-WebCamParam = [640, 360, 15] # 160*120 - min HD - max
-Camera = WebCam(0, WebCamParam[0], WebCamParam[1], WebCamParam[2])
+print(str(setStr[2]))
+# not normal solution
+if 'auto_on' in str(setStr[2]):
+	autoMode = True
+	# print("True")
+else:
+	autoMode = False
+	# print("False")
 
-# WebCamParam = [640, 480]
-# Camera = WebCam(0, WebCamParam[0], WebCamParam[1])
+if 'img_save_on' in str(setStr[3]):
+	autoImgSave = True
+else:
+	autoImgSave = False
 
 mainWindow = View("MachineImprovements", WebCamParam[0], WebCamParam[1])
 ReadOrSaveImg = imgRW.ImgRW()
 flag = True
 
-#
+
 # movement detection init with the equal data
-# frame = Camera.takeFrame().copy() # movement 21_11_18
-# MD = motion.MotionDetect(frame, frame)
-# flagMovement = False
-# moveTime = 0
+frame = Camera.takeFrame().copy() # movement 21_11_18
+MD = motion.MotionDetect(frame, frame)
+flagMovement = False
+moveTime = 0
 
 # gpio
 if os.name == 'posix':
@@ -91,25 +107,40 @@ while mainWindow.getWindowProperty() and flag: # while True:
 	#
 
 	# hide square sole pos
-	if flag == 3:  # ord("d") default square (from settings file)
+	if flag == 3 or autoMode:  # ord("d") default square (from settings file) # autoMode (auto_on) from conf.set
 		mainWindow.loadDefaultSquare(frame, int(setStr[1][0]), int(setStr[1][1]), int(setStr[1][2]), int(setStr[1][3]))
-		print("\'D\'" + ' ' + "Download square pos from set file")
+
+		# better to make something with View draw() func
+		if not autoMode: # if auto we don't need to print "Download squar..." every time
+			print("\'D\'" + ' ' + "Download square pos from set file")
+		# autoMode = False
 	#
 
 	if flag == 4: # save square pos (to settings)
 		kok = mainWindow.returnRefPt()
 		if kok != 0:
-			Set.save('12,45|' + str(kok[0][0]) + ',' + str(kok[0][1]) + ',' + str(kok[1][0]) + ',' + str(kok[1][1])) # [(x1, y1), (x2, y2)])
+			if autoImgSave and autoMode: # for auto mod img save according conf.set
+				tmpStr = "|auto_on|img_save_on"
+			elif not autoImgSave and autoMode:
+				tmpStr = "|auto_on|img_save_off"
+			else:
+				tmpStr = "|auto_off|img_save_on"
+			Set.save(str(frame.shape[1]) + ',' + str(frame.shape[0]) + '|' + str(kok[0][0]) + ',' + str(kok[0][1]) + ',' +
+					 str(kok[1][0]) + ',' + str(kok[1][1]) + tmpStr) # [(x1, y1), (x2, y2)])
+
+			# Set.save(str(frame.shape[1]) + ',' + str(frame.shape[0]) + '|' + str(kok[0][0]) + ',' + str(kok[0][1]) + ',' +
+			# 	str(kok[1][0]) + ',' + str(kok[1][1]) + "|auto_off|img_save_on")  # [(x1, y1), (x2, y2)])
+
 			print("\'S\'" + ' ' + "Save square")
 		else:
 			print("Nothing to save!")
-	#
 
-	# if flag == 5: # turn on/off movement detection
-	# 	flagMovement = not flagMovement
-	# 	print("\'M\'" + " " + str(flagMovement))
-	# 	# print(chr(27) + "[2J") # clear terminal maybe in linux
-	# 	os.system('cls' if os.name == 'nt' else 'clear') # clear terminal
+
+	if flag == 5: # turn on/off movement detection
+		flagMovement = not flagMovement
+		print("\'M\'" + " " + str(flagMovement))
+		# print(chr(27) + "[2J") # clear terminal maybe in linux
+		os.system('cls' if os.name == 'nt' else 'clear') # clear terminal
 
 		if 'soleImg' in locals():
 			MD.loadF1(soleImg)  # load zone
@@ -118,22 +149,22 @@ while mainWindow.getWindowProperty() and flag: # while True:
 			MD.loadF1(frame)  # load zone
 			MD.loadF2(frame)  # load zone
 
-	# if flagMovement: #movement 21_11_18
-	# 	if 'soleImg' in locals():
-	# 		movementStr = MD.loadF2(soleImg)  # frame
-	# 	else:
-	# 		movementStr = MD.loadF2(frame)  # frame
-	#
-	# 	if movementStr == -1: # not 5 iteration passed
-	# 		continue
-	# 	elif movementStr == 1: # movement detected
-	# 		moveTime = time.time()
-	# 		print("Movement! %s" % moveTime)
-	# 		continue
-	# 	elif movementStr == 0: # movement not detected
-	# 		if (time.time() - moveTime) > 0.5:
-	# 	# 		print("Ready!")
-	# 			pass
+	if flagMovement: #movement 21_11_18
+		if 'soleImg' in locals():
+			movementStr = MD.loadF2(soleImg)  # frame
+		else:
+			movementStr = MD.loadF2(frame)  # frame
+
+		if movementStr == -1: # not 5 iteration passed
+			continue
+		elif movementStr == 1: # movement detected
+			moveTime = time.time()
+			print("Movement! %s" % moveTime)
+			continue
+		elif movementStr == 0: # movement not detected
+			if (time.time() - moveTime) > 0.5:
+		# 		print("Ready!")
+				pass
 
 
 
@@ -160,15 +191,14 @@ while mainWindow.getWindowProperty() and flag: # while True:
 					  + str(localTime[0]) + '-' + str(localTime[3]) + '-' + str(localTime[4]) + '-' \
 					  + str(localTime[5]) + '-' + isSoleStr + '-' + 'QR' + '-' + str(barCodeData[1])  # 'IMGs/' +
 
-		ReadOrSaveImg.rw('W', '../IMGs/' + saveImgName + '.png', frame)  # save to img with imgName date + .png
-
-		print('%s is saved' % saveImgName)
+		if autoImgSave: # auto save img according to conf
+			ReadOrSaveImg.rw('W', '../IMGs/' + saveImgName + '.png', frame)  # save to img with imgName date + .png
+			print('%s is saved' % saveImgName)
+		else:
+			print('%s' % saveImgName)
 
 		elapsed_time = time.time() - start_time
 
 		print("Iteration score: %f" % elapsed_time)
-
-		# if len(mainWindow.refPt) == 2:
-		# 	print(mainWindow.refPt)
 
 		print(mainWindow.returnRefPt())
