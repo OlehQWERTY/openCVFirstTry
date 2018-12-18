0e87af0657bce29ad1157fcd417f4359d4616b1c
Unknown <oleggordiichenko@gmail.com>
2018-12-18 13:02:29 +0200

    continue creation: db class  and wrapper for it

--- openCVlib/SoleGluingMachineImpruvements/app/controllers/dbDataProc.py
+++ openCVlib/SoleGluingMachineImpruvements/app/controllers/dbDataProc.py
@@ -9,41 +9,70 @@ from db import Db
 
 log = Debug(True, __name__)  # turn on/off debugging messages in this module
 
-DB = Db("monitor", "password", "localhost", "sole_1")
+DB = Db("monitor1", "password", "localhost", "sole_1")
+
 # DB.req(66, 'Nasty', 101, 'Zoya Semenovna', '4925NG_Poland', '2564', '197')
 # DB.req()
 # DB.getSelectData()
 
+del DB # close connection
+
 class DbDataProc():
-    def __init__(self, path):
-        self.soleArtDict = {}
-        self.queue = []
-        # self.path = path
-        # self.data_new = None
-        # self.data = None
-        pass
-    def createBunch(self, articul):
-        self.soleArtDict[articul] = 0
+	def __init__(self, path):
+		self.soleArtDict = {}
+		self.queue = []
+		# self.path = path
+		# self.data_new = None
+		# self.data = None
 
-    def addToBunch(self, articul):
-        self.soleArtDict[articul] += 1
+	def createBunch(self, articul):
+		self.soleArtDict[articul] = 0
 
-    def trySendToDb(self):
-        # form list for sending to db onece in 5 minutes + save to file 1 per 20 s
-    	if self.soleArtDict:  # if not empty
-            for element in self.soleArtDict:
-                for x in range(int(element / 10)):
-                    self.soleArtDict[element] -= 10
-                    self.queue.append(str(element))  # key check if here we'll get key name not value
+	def addToBunch(self, articul):
+		self.soleArtDict[articul] += 1
 
+	def trySendToDb(self):
+		# form list for sending to db onece in 5 minutes + save to file 1 per 20 s
+		if self.soleArtDict:  # if not empty
+			for element in self.soleArtDict.keys():
+				for x in range(int(self.soleArtDict[element] / 10)):
+					self.queue.append({element:str(self.soleArtDict[element])})  # key check if here we'll get key name not value
+					self.soleArtDict[element] -= 10
+					# print(self.soleArtDict)
 
+	# def setPath(self, path):
+		# self.path = path
 
+	def getQueue(self):
+		# print(self.queue)
+		return self.queue.copy()
 
-    # def setPath(self, path):
-    #     self.path = path
+	def delFromQueue(self):
+		# for x in range(number):
+		print(self.queue.pop())  # "Nasty"
 
 
 
 
 if __name__ == '__main__':
-    print("koko")
+	# print("koko")
+
+	DbProc = DbDataProc("tempSoleDb.mdb")
+
+	DbProc.createBunch("Nasty")
+
+	for x in range(105):
+		DbProc.addToBunch("Nasty")
+
+	DbProc.trySendToDb()
+
+	print(DbProc.getQueue())
+
+	print("\n\n")
+
+	DbProc.delFromQueue()
+
+	print("\n\n")
+
+	print(DbProc.getQueue())
+
--- openCVlib/SoleGluingMachineImpruvements/app/models/db.py
+++ openCVlib/SoleGluingMachineImpruvements/app/models/db.py
@@ -1,12 +1,20 @@
 # sudo pip3 install cython && sudo pip3 install cymysql
 
+# basic mySQL DB requests
+# I didn't add del method for preventing errors in my app and data losing
+
 import cymysql
 import time
 
-# sys.path.append('models')
-# from settings import Settings
-# Set = Settings('temp.SQL_DB')
-# settings_dict = Set.load()
+from sys import exit
+
+#!!!replace all print with log.log()!!!
+
+# sys.path.append('../models')
+# from debug import Debug
+# log = Debug(True, __name__)  # turn on/off debugging messages in this module
+# log.log("Please check SQL server connection", error, __name__)
+
 
 class Db:
     def __init__(self, user, passwd, host, db):
@@ -14,56 +22,86 @@ class Db:
         self.passwd = passwd
         self.host = host
         self.db = db
-
         # conn = cymysql.connect(user="monitor", passwd="password", host="localhost", db="sole_1")
-        self.conn = cymysql.connect(user=self.user, passwd=self.passwd, host=self.host, db=self.db)
-        self.cur = self.conn.cursor()
 
-    def reqStandart(self):
+    def connect(self):
+        result = False
+        try:
+            self.conn = cymysql.connect(user=self.user, passwd=self.passwd, host=self.host, db=self.db)
+        # read about possible exceptions: https://github.com/nakagami/CyMySQL/blob/master/cymysql/err.py
+        except cymysql.err.MySQLError as error:
+            print("Please check SQL server connection", error)
+            # exit()
+        except Exception as error:  # be carefull it could grub other exception
+            code, message = error.args
+            print("Other DB error", code, message)
+            # exit()
+        else:  # if no exception
+            self.cur = self.conn.cursor()
+            result = True
+        finally:
+            # pass  # perform in eny case
+            return result
+
+    def close(self):
+        self.cur.close()
+
+    def reqStandart(self):  # only for debugging
         self.cur.execute("""INSERT INTO glueMachine3 (UnitID,Articul,ProcessID,OperatorName,OperationDate,Pull,OrderNumber,LocalNumber)
             VALUES(66,'Nasty',101,'Zoya Semenovna','17/11/18 17:25:36','4925NG_Poland','2564','197')""")
         self.conn.commit()
 
-    def simpleReq(self, string):
-        self.cur.execute(string)
-        self.conn.commit()
+    def customReq(self, string):  # "standart sql requestr"
+        if self.connect():
+            self.cur.execute(string)
+            self.conn.commit()
+
+    def __request(self, varUnitID, varArticul, varProcessID, varOperatorName, varPull, varOrderNumber, varLocalNumber):
+        if self.connect():
+            localTime = time.localtime(time.time())  # time str
+            saveTime = str(localTime[2]) + '-' + str(localTime[1]) + '-' \
+                       + str(localTime[0]) + '-' + str(localTime[3]) + '-' + str(localTime[4]) + '-' \
+                       + str(localTime[5])
+
+            strA = """INSERT INTO glueMachine3 (UnitID,Articul,ProcessID,OperatorName,OperationDate,Pull,OrderNumber,LocalNumber) VALUES(""" \
+                   + str(varUnitID) + ",\'" + str(varArticul) + "\',\'" + str(varProcessID) + "\',\'" + str(
+                varOperatorName) + "\',\'" \
+                   + saveTime + "\',\'" + str(varPull) + "\',\'" + str(varOrderNumber) + "\',\'" + str(
+                varLocalNumber) + "\')"
+
+            self.cur.execute(strA)
+            # """INSERT INTO glueMachine3 (UnitID,Articul,ProcessID,OperatorName,OperationDate,Pull,OrderNumber,LocalNumber)
+            # VALUES(66,'Nasty',101,'Zoya Semenovna','17/11/18 17:25:36','4925NG_Poland','2564','197')"""
+            self.conn.commit()
 
     def req(self, varUnitID, varArticul, varProcessID, varOperatorName, varPull, varOrderNumber, varLocalNumber):
-        localTime = time.localtime(time.time())  # time str
-        saveTime = str(localTime[2]) + '-' + str(localTime[1]) + '-' \
-                   + str(localTime[0]) + '-' + str(localTime[3]) + '-' + str(localTime[4]) + '-' \
-                   + str(localTime[5])
+        if self.connect():
+            self.__request(varUnitID, varArticul, varProcessID, varOperatorName, varPull, varOrderNumber, varLocalNumber)
+            self.close()
+        else:
+            # can't connect
+            pass
 
-        strA = """INSERT INTO glueMachine3 (UnitID,Articul,ProcessID,OperatorName,OperationDate,Pull,OrderNumber,LocalNumber) VALUES(""" \
-               + str(varUnitID) + ",\'" + str(varArticul) + "\',\'" + str(varProcessID) + "\',\'" + str(
-            varOperatorName) + "\',\'" \
-               + saveTime + "\',\'" + str(varPull) + "\',\'" + str(varOrderNumber) + "\',\'" + str(
-            varLocalNumber) + "\')"
+    def getAllData(self):
+        if self.connect():
+            self.cur.execute("SELECT * from glueMachine3")
+            for r in self.cur.fetchall():
+                print(r)
+            self.conn.commit()  # It isn't neaded in some cases, but I don't want to get any problems because of it
 
-        # print(strA)
-
-        self.cur.execute(strA)
-        # """INSERT INTO glueMachine3 (UnitID,Articul,ProcessID,OperatorName,OperationDate,Pull,OrderNumber,LocalNumber)
-        # VALUES(66,'Nasty',101,'Zoya Semenovna','17/11/18 17:25:36','4925NG_Poland','2564','197')"""
-        self.conn.commit()
-
-    def getSelectData(self, fieldNumber=-1):
-        self.cur.execute("SELECT * from glueMachine3")
-
-        for r in self.cur.fetchall():
-            # if fieldNumber != -1:
-            print(r)
-            # else:
-                # print(r[fieldNumber])
-                # pass
-        self.conn.commit()  # It isn't neaded in some cases, but I don't want to get any problems because of it
+    def detData(self):
+        if self.connect():
+            pass
 
     def __del__(self):
-        self.cur.close()
+        # self.cur.close()  # don't close like this because of it can try to close not opened connection
+        pass
 
 
 if __name__ == '__main__':
     DB = Db("monitor", "password", "localhost", "sole_1")
-    DB.req(66, 'Nasty', 101, 'Zoya Semenovna', '4925NG_Poland', '2564', '197')
-    # DB.req()
-    DB.getSelectData()
+    if DB.connect():
+        DB.req(66, 'Nasty', 101, 'Zoya Semenovna', '4925NG_Poland', '2564', '197')
+        # DB.req()
+        DB.getAllData()
+        DB.close()
